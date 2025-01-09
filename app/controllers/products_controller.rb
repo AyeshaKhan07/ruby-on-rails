c956bfba33e16498aa92172149de53c3076f2d28
AyeshaKhan07 <ayeshanadeem9085@gmail.com>
2025-01-09 14:50:04 +0500

    - Added create and update functionalities for products

--- app/controllers/products_controller.rb
+++ app/controllers/products_controller.rb
@@ -8,6 +8,33 @@ class ProductsController < ApplicationController
   end
 
   def new
-    @product = Product.new
+    @new_product = Product.new
   end
+
+  def create
+    @new_product = Product.new(product_validations)
+    if(@new_product.save)
+      redirect_to @new_product
+    else
+      render :new, status: :unprocessable_entity
+    end
+  end
+
+  def edit
+    @product_to_update = Product.find(params[:id])
+  end
+
+  def update
+    @product_to_update = Product.find(params[:id])
+    if(@product_to_update.update(product_validations))
+      redirect_to @product_to_update
+    else
+      render :edit, status: :unprocessable_entity
+    end
+  end
+
+  private
+    def product_validations
+      params.expect(product: [ :name ])
+    end
 end
